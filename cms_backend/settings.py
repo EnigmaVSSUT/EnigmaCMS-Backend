8aaaea13039ccd7a456e77091ad6df566a0d0e9e
partha2000 <parthaprateem2000@gmail.com>
2020-10-12 11:07:51 +0530

    added users app

--- cms_backend/settings.py
+++ cms_backend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'users.apps.UsersConfig'
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
