e05d7fdd34b8cbe05b5ead2d1846f2fc8365801e
Priyanshu Kumar Singh <priyanshusingh1998@gmail.com>
2021-11-21 21:23:41 +0530

    added the secret_key to credentials.json

--- cms_backend/settings.py
+++ cms_backend/settings.py
@@ -32,7 +32,7 @@ def get_secret(setting,secrets=credentials):
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f1rakded^b%nk#t#gcb9)p$d@ov2%q$3ahk5^(x&esl8f63rk7'
+SECRET_KEY = get_secret('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
