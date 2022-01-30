75e179b3ebc5fafc647fb8ae2e837aca0fae55e8
Priyanshu Kumar Singh <priyanshu.singh@solytics-partners.com>
2022-01-30 13:31:23 +0530

    added the registration in admin page

--- events/admin.py
+++ events/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Event
+from .models import Event, EventRegistration
 # Register your models here.
 admin.site.register(Event)
+admin.site.register(EventRegistration)
+
