e42d1fc8207acb2f74de6ac8d0baa524332be0aa
Ravindra Nag <ravindranag52@gmail.com>
2022-09-26 19:48:16 +0530

    db connect url

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,5 +1,14 @@
 import { PrismaClient } from "@prisma/client";
+import * as dotenv from 'dotenv'
 
-const prisma: PrismaClient = new PrismaClient()
+dotenv.config()
+
+const prisma: PrismaClient = new PrismaClient({
+	datasources: {
+		db: {
+			url: process.env.DATABASE_URL
+		}
+	}
+})
 
 export default prisma
