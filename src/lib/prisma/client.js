9e1bf257a5fd7a64583aa973ae73af6762d14d7e
ravindranag <ravindranag52@gmail.com>
2023-05-22 05:19:10 +0530

    fix(prisma-client): import path

--- src/lib/prisma/client.js
+++ src/lib/prisma/client.js
@@ -1,5 +1,4 @@
-// import { PrismaClient } from "@prisma/client";
-import { PrismaClient } from '../../../prisma/client/index.js'
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
