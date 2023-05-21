cff80ea1c2a1fc440903a981514084fe04745288
ravindranag <ravindranag52@gmail.com>
2023-05-21 22:49:46 +0530

    build: put prisma client out of node_modules

--- src/lib/prisma/client.js
+++ src/lib/prisma/client.js
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '../../../prisma/client/index.js'
 
 const prisma = new PrismaClient()
 
