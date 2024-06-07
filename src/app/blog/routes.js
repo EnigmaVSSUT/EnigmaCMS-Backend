b474619c22ebe25d175d69d4efd67b22b6826905
Vishal Ekka <vishalekka18@gmail.com>
2024-06-07 22:27:43 +0530

    Blogs and Profile backend updated

--- src/app/blog/routes.js
+++ src/app/blog/routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { saveBlog } from "./controller.js";
-
+import { getBlogs } from './controller.js';
 
 const router = Router()
 
 router.post('/create', saveBlog)
-// router.get('/', getAllInductionController)
+router.get('/getBlogs', getBlogs);
 
 export default router;
